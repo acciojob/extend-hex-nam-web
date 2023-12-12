a191452c6199992facb2ad10a9aa8e846c6ae425
nam-web <65065658+nam-web@users.noreply.github.com>
2023-12-12 12:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 shortHex = shortHex.replace(/^#/, '');
+
+  // Split the short hex code into color components
+  const r = shortHex[0];
+  const g = shortHex[1];
+  const b = shortHex[2];
+
+  // Extend each color component to two characters
+  const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
 };
 
 // Do not change the code below.
